8f2b2fe3b69a6aafa47bf3009ecbb0a8ba1f0e1a
Annie <68303208+anniestjernedryss@users.noreply.github.com>
2020-08-26 16:54:08 -0400

    updated celsius conversion link

--- src/index.js
+++ src/index.js
@@ -21,14 +21,13 @@ let todayDate = new Date();
 document.getElementById("date").innerHTML = formatDate(todayDate);
 
 function showTemperature(response) {
-  celciusTemperature = response.data.main.temp;
+  celsiusTemperature = response.data.main.temp;
   document.getElementById("temperature-now").innerHTML = Math.round(
     response.data.main.temp
   );
   document.getElementById("city-name").innerHTML = response.data.name;
   document.getElementById("weather-description").innerHTML =
     response.data.weather[0].description;
-  document.getElementById("celsius-link").innerHTML = "°C";
   document.getElementById("humidity-description").innerHTML =
     response.data.main.humidity;
   document.getElementById("wind-description").innerHTML =
@@ -54,14 +53,13 @@ function clickForPosition(event) {
 let locationNow = document.querySelector("#location-now");
 locationNow.addEventListener("click", clickForPosition);
 function showNewTemperature(response) {
-  celciusTemperature = response.data.main.temp;
+  celsiusTemperature = response.data.main.temp;
   document.getElementById("temperature-now").innerHTML = Math.round(
     response.data.main.temp
   );
   document.getElementById("city-name").innerHTML = response.data.name;
   document.getElementById("weather-description").innerHTML =
     response.data.weather[0].description;
-  document.getElementById("celsius-link").innerHTML = "°C";
   document.getElementById("humidity-description").innerHTML =
     response.data.main.humidity;
   document.getElementById("wind-description").innerHTML =
@@ -84,14 +82,22 @@ function findNewLocation(position) {
 let searchEngine = document.querySelector("#search-engine");
 searchEngine.addEventListener("submit", findNewLocation);
 
+let celsiusTemperature = null;
 function showTemperatureinF(event) {
   event.preventDefault();
   let temperatureNow = document.querySelector("#temperature-now");
-  let farenheitTemp = (celciusTemperature * 9) / 5 + 32;
+  let farenheitTemp = (celsiusTemperature * 9) / 5 + 32;
   temperatureNow.innerHTML = Math.round(farenheitTemp);
 }
 
 let farenheitLink = document.querySelector("#fahrenheit-link");
 farenheitLink.addEventListener("click", showTemperatureinF);
 
-let celciusTemperature = null;
+function showTemperatureInC(event) {
+  event.preventDefault();
+  let temperatureNow = document.querySelector("#temperature-now");
+  temperatureNow.innerHTML = Math.round(celsiusTemperature);
+}
+
+let celsiusLink = document.querySelector("#celsius-link");
+celsiusLink.addEventListener("click", showTemperatureInC);
